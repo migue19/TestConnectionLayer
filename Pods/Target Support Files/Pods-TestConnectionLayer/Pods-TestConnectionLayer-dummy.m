d7c05b5ccf8e91553e9392278a2739ba2c6497fc
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2020-10-17 15:15:48 -0500

    primer commit

---
+++ Pods/Target Support Files/Pods-TestConnectionLayer/Pods-TestConnectionLayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestConnectionLayer : NSObject
+@end
+@implementation PodsDummy_Pods_TestConnectionLayer
+@end
--- TestConnectionLayer/ViewController.swift
+++ TestConnectionLayer/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
-
+import ConnectionLayer
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testConnectionLayer()
     }
 
+    func testConnectionLayer(){
+        let connection = ConnectionLayer()
+        connection.conneccionRequest(url: <#T##String#>, method: <#T##String#>, headers: <#T##[String : String]#>, parameters: <#T##[String : Any]?#>, closure: <#T##(Data?, String?) -> Void#>)
+    }
 
 }
 
